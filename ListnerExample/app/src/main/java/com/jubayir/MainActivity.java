1453c7939c1da237a2f3cd7d12e451c069b144c3
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-07-05 18:26:18 +0600

    Listner_Example_MainActivity

--- ListnerExample/app/src/main/java/com/jubayir/MainActivity.java
+++ ListnerExample/app/src/main/java/com/jubayir/MainActivity.java
@@ -2,12 +2,26 @@ package com.jubayir;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button loginBtn, logoutBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginBtn = findViewById(R.id.loginBtn);
+        logoutBtn = findViewById(R.id.logoutBtn);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
