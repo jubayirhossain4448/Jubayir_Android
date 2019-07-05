6a170e45787c568d6fbd56a254b007da9b39a92e
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-07-05 18:37:52 +0600

    Listner_Example_MainActivity!

--- ListnerExample/app/src/main/java/com/jubayir/MainActivity.java
+++ ListnerExample/app/src/main/java/com/jubayir/MainActivity.java
@@ -1,13 +1,16 @@
 package com.jubayir;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button loginBtn, logoutBtn;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +20,20 @@ public class MainActivity extends AppCompatActivity {
         loginBtn = findViewById(R.id.loginBtn);
         logoutBtn = findViewById(R.id.logoutBtn);
 
-        loginBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        Handler handler = new Handler();
 
+        loginBtn.setOnClickListener(handler);
+        logoutBtn.setOnClickListener(handler);
+    }
+    class Handler implements View.OnClickListener{
+
+        @Override
+        public void onClick(View v) {
+            if (v.getId() == R.id.loginBtn){
+                textView.setText("Login Button is Clicked ");
+            }else if (v.getId() == R.id.logoutBtn){
+                textView.setText("Logout Button is Clicked ");
             }
-        });
+        }
     }
 }
