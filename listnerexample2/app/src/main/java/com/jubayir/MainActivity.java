46e2427bae963910f753fff2fd510ccd93f2f564
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-07-07 23:37:36 +0600

    Listner_activity_main

--- listnerexample2/app/src/main/java/com/jubayir/MainActivity.java
+++ listnerexample2/app/src/main/java/com/jubayir/MainActivity.java
@@ -2,12 +2,26 @@ package com.jubayir;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button loginBtn, logoutBtn;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginBtn = findViewById(R.id.loginBtn);
+        logoutBtn = findViewById(R.id.logoutBtn);
+        textView = findViewById(R.id.textView);
+
+    }
+
+    public void showMessage(){
+        textView.setText("Login button is Clicked");
     }
 }
