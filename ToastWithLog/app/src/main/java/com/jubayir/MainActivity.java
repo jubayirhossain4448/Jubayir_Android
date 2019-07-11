00b3c9256626d40b9d232c551dbc36a2fb2fee2e
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-07-11 18:58:20 +0600

    Log_example

--- ToastWithLog/app/src/main/java/com/jubayir/MainActivity.java
+++ ToastWithLog/app/src/main/java/com/jubayir/MainActivity.java
@@ -2,24 +2,35 @@ package com.jubayir;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button loginBtn, LogoutBtn;
+    private Button loginBtn, logoutBtn;
     private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginBtn = findViewById(R.id.loginBtn);
+        logoutBtn = findViewById(R.id.logoutBtn);
+        textView = findViewById(R.id.textView);
     }
 
-    public void loginMessage(View view) {
+    public void loginMessage(View v) {
+
+            Log.d("tag", "Login Button is clicked"); // tag == key, msg = value
+
+            
+
     }
 
     public void logoutMessage(View view) {
+        Log.d("tag", "Logout Button is clicked"); // tag == key, msg = value
     }
 }
