b8d7e6270fac883eaac341fa6f3adcd2ab895b98
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-07-11 17:14:22 +0600

    MainActivity

--- toastexample3/app/src/main/java/com/jubayir/MainActivity.java
+++ toastexample3/app/src/main/java/com/jubayir/MainActivity.java
@@ -3,15 +3,26 @@ package com.jubayir;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button toastBtn;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toastBtn = findViewById(R.id.toastBtn);
+        textView = findViewById(R.id.textView);
+
     }
 
     public void Toast_Message(View view) {
+        Toast.makeText(MainActivity.this, "Toast Example is Shown", Toast.LENGTH_SHORT).show();
     }
 }
