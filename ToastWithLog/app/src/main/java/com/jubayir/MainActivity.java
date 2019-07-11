0f38f11d8cedaf2370c87442c77ebf7d6f81d587
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-07-11 18:47:26 +0600

    Activity_main

--- ToastWithLog/app/src/main/java/com/jubayir/MainActivity.java
+++ ToastWithLog/app/src/main/java/com/jubayir/MainActivity.java
@@ -2,12 +2,24 @@ package com.jubayir;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button loginBtn, LogoutBtn;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void loginMessage(View view) {
+    }
+
+    public void logoutMessage(View view) {
     }
 }
