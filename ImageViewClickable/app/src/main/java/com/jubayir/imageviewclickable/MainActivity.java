4d10010d63d349e4e3d42fce5427734b8001209d
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-07-13 18:07:28 +0600

    Custom_Toast

--- ImageViewClickable/app/src/main/java/com/jubayir/imageviewclickable/MainActivity.java
+++ ImageViewClickable/app/src/main/java/com/jubayir/imageviewclickable/MainActivity.java
@@ -2,12 +2,32 @@ package com.jubayir.imageviewclickable;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private ImageView imageView1, imageView2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageView1 = findViewById(R.id.imageView1Id);
+        imageView2 = findViewById(R.id.imageView2Id);
+
+        imageView1.setOnClickListener(this);
+        imageView2.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if (v.getId()== R.id.imageView1Id){
+            Toast.makeText(MainActivity.this, "Md. Jubayir", Toast.LENGTH_SHORT).show();
+        }else if (v.getId()== R.id.imageView2Id){
+            Toast.makeText(MainActivity.this, "Md. Jubayir Hossain", Toast.LENGTH_SHORT).show();
+        }
+
     }
 }
