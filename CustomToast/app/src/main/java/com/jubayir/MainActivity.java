9e8f0cdeaa2c876e45ffab8aa5ce6b93fb607e71
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-07-13 23:25:03 +0600

    custom_layout_MainActivity

--- CustomToast/app/src/main/java/com/jubayir/MainActivity.java
+++ CustomToast/app/src/main/java/com/jubayir/MainActivity.java
@@ -2,12 +2,34 @@ package com.jubayir;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Button loginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginBtn = findViewById(R.id.loginBtn);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LayoutInflater inflater = getLayoutInflater();
+               View customView = inflater.inflate(R.layout.custom_toast_layout, (ViewGroup) findViewById(R.id.customToastLayout));
+
+                Toast toast = new Toast(MainActivity.this);
+                toast.setDuration(Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.CENTER, 0, 0);
+                toast.setView(customView);
+                toast.show();
+            }
+        });
     }
 }
