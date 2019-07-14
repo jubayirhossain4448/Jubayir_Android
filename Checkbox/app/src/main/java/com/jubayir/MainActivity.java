3fe477045bfc232d268081a9a283fca0ea477c50
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-07-15 01:10:12 +0600

    Checkbox_Main_update

--- Checkbox/app/src/main/java/com/jubayir/MainActivity.java
+++ Checkbox/app/src/main/java/com/jubayir/MainActivity.java
@@ -11,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+        milkChe = findViewById(R.id.milkChk);
+        sugarChe = findViewById(R.id.sugarChk);
+        waterChe = findViewById(R.id.waterChk);
+     }
 }
