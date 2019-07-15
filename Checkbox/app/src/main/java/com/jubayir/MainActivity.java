1970aa542e679722ce7a026384e8bcb601fcc252
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-07-15 22:54:15 +0600

    Checkbox_setOnClickListner

--- Checkbox/app/src/main/java/com/jubayir/MainActivity.java
+++ Checkbox/app/src/main/java/com/jubayir/MainActivity.java
@@ -2,10 +2,15 @@ package com.jubayir;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private CheckBox milkChe, sugarChe, waterChe;
+    private Button showBtn;
+    private TextView resultTV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +20,28 @@ public class MainActivity extends AppCompatActivity {
         milkChe = findViewById(R.id.milkChk);
         sugarChe = findViewById(R.id.sugarChk);
         waterChe = findViewById(R.id.waterChk);
+
+        showBtn = findViewById(R.id.showBtn);
+        resultTV = findViewById(R.id.resultTV);
+
+        showBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                StringBuilder stringBuilder = new StringBuilder();
+                if (milkChe.isChecked()){
+                    String value = milkChe.getText().toString();
+                    stringBuilder.append(value + "Is ordered " + "\n");
+                }
+                if (sugarChe.isChecked()){
+                    String value = sugarChe.getText().toString();
+                    stringBuilder.append(value + "Is ordered " + "\n");
+                }
+                if (waterChe.isChecked()){
+                    String value = waterChe.getText().toString();
+                    stringBuilder.append(value + "Is ordered  " + "\n");
+                }
+                resultTV.setText(stringBuilder);
+            }
+        });
      }
 }
