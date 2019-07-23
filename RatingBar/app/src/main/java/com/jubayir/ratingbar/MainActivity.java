47f02313ca0b41c73f53e9ba865430db173c6499
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-07-24 00:38:13 +0600

    MainActivity

--- RatingBar/app/src/main/java/com/jubayir/ratingbar/MainActivity.java
+++ RatingBar/app/src/main/java/com/jubayir/ratingbar/MainActivity.java
@@ -3,12 +3,29 @@ package com.jubayir.ratingbar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RatingBar ratingBar;
+    private TextView textViewid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ratingBar = findViewById(R.id.ratingBar);
+        textViewid = findViewById(R.id.textViewId);
+        textViewid.setText("Value : " + ratingBar.getProgress());
+
+        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float v, boolean b) {
+
+                textViewid.setText("Value : " + ratingBar);
+            }
+        });
     }
 }
