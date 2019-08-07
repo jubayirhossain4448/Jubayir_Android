f48bed674e360acb5bc4d84c786592ae89275086
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-08-07 23:44:50 +0600

    MainActivity

--- CheckBoxx/app/src/main/java/com/jubayir/checkbox/MainActivity.java
+++ CheckBoxx/app/src/main/java/com/jubayir/checkbox/MainActivity.java
@@ -3,12 +3,23 @@ package com.jubayir.checkbox;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
 
 public class MainActivity extends AppCompatActivity {
+    
+    private CheckBox mailCH, sugarCH, waterCH;
+    private Button showBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        init();
+        
+    }
+
+    private void init() {
     }
 }
