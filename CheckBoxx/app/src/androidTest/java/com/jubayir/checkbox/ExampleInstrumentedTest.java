20c1e57af97e2b8087fef4329cc3421ed71c2622
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-08-07 23:41:17 +0600

    main_activity

--- CheckBoxx/app/src/androidTest/java/com/jubayir/checkbox/ExampleInstrumentedTest.java
+++ CheckBoxx/app/src/androidTest/java/com/jubayir/checkbox/ExampleInstrumentedTest.java
@@ -1,6 +1,7 @@
 package com.jubayir.checkbox;
 
 import android.content.Context;
+
 import androidx.test.InstrumentationRegistry;
 import androidx.test.runner.AndroidJUnit4;
 
--- CheckBoxx/app/src/main/java/com/jubayir/checkbox/MainActivity.java
+++ CheckBoxx/app/src/main/java/com/jubayir/checkbox/MainActivity.java
@@ -1,6 +1,7 @@
 package com.jubayir.checkbox;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
