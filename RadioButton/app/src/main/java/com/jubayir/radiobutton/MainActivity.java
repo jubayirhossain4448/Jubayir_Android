86598a506996f003884e147f089e0fdd2be62533
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-08-08 23:44:49 +0600

    init()

--- RadioButton/app/src/main/java/com/jubayir/radiobutton/MainActivity.java
+++ RadioButton/app/src/main/java/com/jubayir/radiobutton/MainActivity.java
@@ -3,12 +3,29 @@ package com.jubayir.radiobutton;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RadioGroup radioGroup;
+    private RadioButton maleRBtn, femaleRBtn;
+    private Button showBtn;
+    private TextView resultTV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+    }
+
+    private void init() {
+        radioGroup = findViewById(R.id.radionGroup);
+        showBtn = findViewById(R.id.showBtn);
+        resultTV = findViewById(R.id.resultTV);
     }
 }
