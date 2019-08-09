c59596c529baad9235e83864451f0126a68698ce
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-08-10 00:09:33 +0600

    init

--- Switch/app/src/main/java/com/jubayir/aswitch/MainActivity.java
+++ Switch/app/src/main/java/com/jubayir/aswitch/MainActivity.java
@@ -12,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        aSwitch = findViewById(R.id.switchS);
+
+        
     }
 }
