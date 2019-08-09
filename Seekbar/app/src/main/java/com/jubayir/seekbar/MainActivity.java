6f64865896d929bbb3e5f76a94cec04019e1d38b
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-08-09 23:22:17 +0600

    MainActivity

--- Seekbar/app/src/main/java/com/jubayir/seekbar/MainActivity.java
+++ Seekbar/app/src/main/java/com/jubayir/seekbar/MainActivity.java
@@ -3,12 +3,19 @@ package com.jubayir.seekbar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private SeekBar seekBar;
+    private TextView textViewId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        seekBar = findViewById(R.id.seekbar);
+        textViewId = findViewById(R.id.textViewId);
     }
 }
