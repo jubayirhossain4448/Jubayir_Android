55c9262412a042f60804901697009b9e6c511b8e
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-08-10 00:07:35 +0600

    MainActivity

--- Switch/app/src/main/java/com/jubayir/aswitch/MainActivity.java
+++ Switch/app/src/main/java/com/jubayir/aswitch/MainActivity.java
@@ -3,8 +3,10 @@ package com.jubayir.aswitch;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
+    private Switch aSwitch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
