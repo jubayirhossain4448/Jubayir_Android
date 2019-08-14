2e7576a2f1633f36bc787af686bda3f57346f939
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-08-14 23:35:03 +0600

    MainActivity

--- NationalUniversity/app/src/main/java/com/jubayir/nationaluniversity/MainActivity.java
+++ NationalUniversity/app/src/main/java/com/jubayir/nationaluniversity/MainActivity.java
@@ -3,12 +3,19 @@ package com.jubayir.nationaluniversity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
+    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        init();
+    }
+
+    private void init() {
     }
 }
